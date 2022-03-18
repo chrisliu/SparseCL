aea6b714940d80601221ca675254af555740a79d
npochhi <pochhi.nilay@gmail.com>
2022-03-17 17:38:52 -0700

    bug

--- src/mem/cache/tags/base_set_assoc.hh
+++ src/mem/cache/tags/base_set_assoc.hh
@@ -124,7 +124,7 @@ class BaseSetAssoc : public BaseTags
      * @param lat The latency of the tag lookup.
      * @return Pointer to the cache block if found.
      */
-    CacheBlk* accessBlock(const PacketPtr pkt, Cycles &lat, bool isSparse, unsigned blk_size) override
+    CacheBlk* accessBlock(const PacketPtr pkt, Cycles &lat, bool isSparse, unsigned blk_size)
     {
         CacheBlk *blk = findBlock(pkt->getAddr(), pkt->isSecure());
         
